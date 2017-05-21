2316cc7eda15cc9c98a155a236f9e2788be40b1b
Evgeny <je.mouth@gmail.com>
2017-05-22 01:04:59 +0300

    solved euler 116

--- plates/main.cpp
+++ plates/main.cpp
@@ -7,22 +7,77 @@ int count_plates(int plt_sz, int ln){
         return 1;
     int p_ln = 0;
     int sum = 1;
-    while(ln>0){
-         ln-=plt_sz;
-         p_ln+=plt_sz;
+    int _ln = ln/2+1;
+    ln-=plt_sz;
+    while(_ln>0){
          sum+=1;
          sum+=count_plates(plt_sz,ln);
          sum+=count_plates(plt_sz,p_ln);
-
+         ln-=1;
+         p_ln+=1;
+         _ln-=1;
         }
     return sum;
 
 }
 
+class  plt
+    {
+public:
+    int * solutions;
+    int sz;
+    int plt_sz;
+    int ln;
+    plt(int _plt_sz, int _ln) {
+         sz = _ln - _plt_sz+2;
+         plt_sz = _plt_sz;
+         ln =_ln;
+         if (sz>0){
+         solutions = new int[sz];
+         if(solutions == nullptr)
+             return;
+         solutions[0] = 0;
+         solutions[1] = 1;
+         for(int i = 2;i<sz;++i)
+            solutions[i] = -1;
+         }
+         for(int i = 0;i<sz;++i)
+            std::cout<<solutions[i]<<" ";
+
+    }
+
+    int get_index(int ps,int ln){
+        if(ps>ln)
+            return 0;
+        return ln-ps+1;
+    }
+
+   int solve(int ps, int ln){
+         int ind = get_index(ps,ln);
+         if(solutions[ind] != -1)
+             return solutions[ind];
+         int sol = 0 ; // solutions counter
+         int _ln = ln/2+1; // steps
+         int p = 0;
+         while(ln-ps-p>=0 && _ln>0){
+                 sol+=1;
+                 sol+=solve(ps,p)+solve(ps,ln-p-ps);
+                 ++p;
+                 --_ln;
+             }
+         solutions[ind] = sol;
+         return sol;
+     }
+
+
+
+
+    };
 
 
 int main(int argc, char *argv[])
 {
-    std::cout<<count_plates(2,5);
-    return 0;
+    plt p(3,5);
+    std::cout<<p.solve(3,5);
+
 }
